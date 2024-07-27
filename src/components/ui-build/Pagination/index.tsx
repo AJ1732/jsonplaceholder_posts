6bb819effcb13771b3c9f7ad7cffee59a267ceb7
AJ1732 <ejemeniboi@gmail.com>
2024-07-27 13:11:13 +0100

    chore: implement pagination

--- src/components/ui-build/Pagination/index.tsx
+++ src/components/ui-build/Pagination/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Pagination,
   PaginationContent,
@@ -6,33 +7,44 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "@/components/ui/pagination";
+import { useSearchParams } from "next/navigation";
 
 export default function PaginationDemo() {
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page") ?? "1";
+  const limit = searchParams.get("limit") ?? "5";
+
   return (
     <Pagination>
       <PaginationContent>
         <PaginationItem>
-          <PaginationPrevious href="#" />
+          <PaginationPrevious
+            href={`/?page=${Number(page) - 1}&limit=${limit}#posts`}
+          />
         </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
+
+        {[1, 2, 3].map((item) => (
+          <PaginationItem>
+            <PaginationLink
+              href={`/?page=${item}&limit=${limit}#posts`}
+              isActive={Number(page) === item}
+            >
+              {item}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
         <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
+
         <PaginationItem>
-          <PaginationNext href="#" />
+          <PaginationNext
+            href={`/?page=${Number(page) + 1}&limit=${limit}#posts`}
+          />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
-  )
+  );
 }
